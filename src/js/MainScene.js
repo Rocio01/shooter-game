f0212fdce13fb6fab01b610e85f6d29d8f88f983
exit <zrmartinezg@gmail.com>
2021-07-01 15:06:42 -0500

    Add Bacground to the game

--- src/js/MainScene.js
+++ src/js/MainScene.js
@@ -2,6 +2,14 @@ class MainScene extends Phaser.Scene {
   constructor() {
     super({ key: "MainScene" });
   }
+
+  preload() {
+    this.load.image("blue", "src/assets/Background/blue.jpg")
+  }
+
+  create() {
+    this.add.image(400, 300, "blue");
+  }
 }
 
 export {MainScene}
