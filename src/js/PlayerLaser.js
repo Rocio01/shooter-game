f5a488690e8c751f60ad418e722d44b363bd3dd8
exit <zrmartinezg@gmail.com>
2021-07-01 17:36:29 -0500

    Add PlayerLaser class with constructor method

---
+++ src/js/PlayerLaser.js
@@ -0,0 +1,13 @@
+class PlayerLaser extends Phaser.GameObjects.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y);
+    this.setTexture('laser');
+    this.setPosition(x, y);
+    this.speed = 10;
+    this.scene = scene;
+    scene.physics.world.enable(this);
+  }
+
+
+}
+
