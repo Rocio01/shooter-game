fd4c034ed6eedb9d8f690399813514bd4a4c8a78
Zulma Rocio <zrmartinezg@gmail.com>
2021-07-14 17:57:20 -0500

    Add display name functionality on game over scene

--- src/js/scenes/GameOverScene.js
+++ src/js/scenes/GameOverScene.js
@@ -13,19 +13,41 @@ class GameOverScene extends Phaser.Scene {
     const { width } = this.scale;
     const { height } = this.scale;
     this.add.text(width * 0.5, height * 0.5, 'Game Over', { fontSize: 48 }).setOrigin(0.5);
-    this.add.text(width * 0.5, height * 0.6, 'Your Score: ' + this.finalScore, { fontSize: 48 }).setOrigin(0.5);
+    
+    
     this.input.keyboard.once('keydown-SPACE', () => {
       this.scene.start('game')
-       })
+       });
   
       const text = this.add.text(this.game.config.width * 0.5, 40, 'Please enter your name', { color: 'white', fontSize: '42px ' });
       text.setOrigin(0.5);
       const form = `<input type="text" name="nameField" placeholder="Enter your name" style="font-size: 32px">
-      <input type="button" name="playButton" value="Save" style="font-size: 32px">`;
+      <input type="button" name="saveButton" value="Save" style="font-size: 32px">`;
   
       const element = this.add.dom(this.game.config.width * 0.5, 100).createFromHTML(form);
       element.setOrigin(0.5);
       element.addListener('click');
+
+
+      element.on('click', (event) => {
+        if (event.target.name === 'saveButton') {
+           const inputText = element.getChildByName('nameField');
+     
+          
+          if (inputText.value !== '') {
+            
+            element.removeListener('click');
+  
+            element.setVisible(false);
+
+            this.playerName = inputText.value;
+            this.add.text(width * 0.5, height * 0.6, this.playerName + ' your score: ' + this.finalScore, { fontSize: 48 }).setOrigin(0.5);
+    
+          } 
+          this.playerName;
+        }
+        
+      });
   
   }
 
@@ -33,3 +55,7 @@ class GameOverScene extends Phaser.Scene {
 }
 
 export default GameOverScene;
+
+
+
+ 
